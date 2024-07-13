f9d82c1fccab52576362a80a422e5088d0eba065
Porosh Mondal <porosh7799@gmail.com>
2024-07-13 23:41:44 +0600

    Implement the Binary Search using C code
    Due:
    - implementation the logic of binary search
    - algorithm writing in readme file

---
+++ arrays/binary_serach/binary_serach.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+
+    //int la[100], LB, UB, n, LOC;
+
+    int array[] = {3, 4, 5, 6, 7, 8, 9};
+    int n = sizeof(array) / sizeof(array[0]);
+    int x = 4;
+
+    return 0;
+}
