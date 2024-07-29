92ca3e168041c8fa073ef973dc7435138064c565
Porosh Mondal <porosh7799@gmail.com>
2024-07-29 23:41:34 +0600

    implement the matrix input functionality

--- arrays/multidimensional_array/matrix_multiplication.c
+++ arrays/multidimensional_array/matrix_multiplication.c
@@ -5,10 +5,37 @@ int main(){
 
     printf("Matrix Multiplication....");
 
+    printf("Enter the number of rows for Matrix A: ");
+    scanf("%d",&M);
+
+    printf("Enter the number of columns for Matrix A and number of rows for Matrix B: ");
+    scanf("%d",&P);
+
+    printf("Enter the number columns for Matrix B: ");
+    scanf("%d",&N);
+
+    // input elements for Matrix A
+    inputMatrixEntry(A, M, P);
+
+    // input elements for Matrix B
+    //inputMatrixEntry(B, P, N);
+
+    // output the Multiplication of AB
+    result(A, M, P);
+
     return 0;
 }
 
 void inputMatrixEntry(int matrix[10][10], int row, int column){
+
+    printf("\nEnter matrix elements: \n\n");
+
+    for(int i=0; i<row; i++){
+        for(int j=0; j<column; j++){
+            printf("Enter the entry for %d%d: ", i + 1, j + 1);
+            scanf("%d",&matrix[i][j]);
+        }
+    }
 
 }
 
@@ -17,5 +44,13 @@ void multiplyMatrix(int A[10][10], int B[10][10], int C[10][10], int M, int P, i
 }
 
 void result(int C[10][10], int M, int N){
-
+    printf("\nThe result of Matrix Multiplication of A*B is: \n");
+    for(int i=0; i<M; i++){
+        for(int j=0; j<N; j++){
+            printf("%d ", C[i][j]);
+            if(j == N - 1){
+                printf("\n");
+            }
+        }
+    }
 }
