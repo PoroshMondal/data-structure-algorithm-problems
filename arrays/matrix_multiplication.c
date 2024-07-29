1b58fb18e00498046c473369792a128b5f87d15d
Porosh Mondal <porosh7799@gmail.com>
2024-07-29 17:48:03 +0600

    Implementing the Matrix Multiplication Algorithm using C

---
+++ arrays/matrix_multiplication.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+    int A[10][10], B[10][10], C[10][10], M, P, N;
+
+    printf("Matrix Multiplication....");
+
+    return 0;
+}
+
+void inputMatrixEntry(int matrix[10][10], int row, int column){
+
+}
+
+void multiplyMatrix(int A[10][10], int B[10][10], int C[10][10], int M, int P, int N){
+
+}
+
+void result(int C[10][10], int M, int N){
+
+}
